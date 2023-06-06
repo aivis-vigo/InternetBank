f7b4f454c5892058986c0226c304c7ddc9576499
aivis-vigo <aivisvigoreimarts@gmail.com>
2023-06-06 09:58:47 +0300

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Session;
-
 class UserController extends Controller
 {
     public function index()
